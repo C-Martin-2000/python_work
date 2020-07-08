3d7db64f59ad4ec1d0f6993f65400e4d4acfba8a
hotrockcoffeedotcom <christmartin83@gmail.com>
2020-07-08 19:44:02 -0400

    all

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+print("hello world!")
+print("test test test")
+variable = "here it is "
+print(variable)
+stuff = "whatever whatever whatever"
+print(variable + stuff)
+stuff = "poop poop"
---
+++ strings_with_methods.py
@@ -0,0 +1,2 @@
+name = "bilbo baggins"
+print(name.title())
